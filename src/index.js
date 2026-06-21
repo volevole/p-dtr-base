ede67da97503ac2bb6bdc91389a9a8e147c83254
Vole <there@rambler.ru>
2026-06-21 11:04:37 +0300

    Добавлен PWA и Service worker

--- src/index.js
+++ src/index.js
@@ -4,16 +4,26 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // ?? ДОБАВИТЬ
 
-import { BrowserRouter } from 'react-router-dom'; // ? импортируем
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter> {/* ? оборачиваем */}
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
 );
 
+// ?? ЗАМЕНИТЬ reportWebVitals() на это:
+// Если хотите включить Service Worker (PWA):
+serviceWorkerRegistration.register();
+
+// Если хотите оставить как было (без PWA):
+// serviceWorkerRegistration.unregister();
+// reportWebVitals();
+
+// Если нужны и PWA, и reportWebVitals:
 reportWebVitals();
